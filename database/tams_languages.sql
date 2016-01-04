a5bd8f307245bd68306a4be6cace3b4fe3fe9b41
Kafia Ahmed <kafia@sutlej.net>
2016-01-04 14:15:06 +0500

    TAMS languages DB

---
+++ database/tams_languages.sql
@@ -0,0 +1,16 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.6.21 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `tams_languages` (
+	` Q;` int (11),
+	`øQ;` varchar (150),
+	`PR;` varchar (150),
+	`¨R;` varchar (150),
+	`` date ,
+	`XS;` varchar (150),
+	`ÀS;` date 
+); 
