048d29a9dd36aee9434cb50cc70fb2b321ce7a0d
Waseem Mehmood <waseem@sutlej.net>
2016-01-06 12:35:43 +0500

    Form designing
    Designing GUI for add talent

--- dashboard/modules/talent/add_talent.php
+++ dashboard/modules/talent/add_talent.php
@@ -1,3 +1,94 @@
 <?php
-echo "Add new talent";
+		
 ?>
+<!--style>
+	div.xAxis div.tickLabel 
+{    
+    transform: rotate(-45deg);
+    -ms-transform:rotate(-45deg); /* IE 9 */
+    -moz-transform:rotate(-45deg); /* Firefox */
+    -webkit-transform:rotate(-45deg); /* Safari and Chrome */
+    -o-transform:rotate(-45deg); /* Opera */
+    /*rotation-point:25% 25%;*/ /* CSS3 */
+    /*rotation:270deg;*/ /* CSS3 */
+}
+	
+</style -->
+<!-- Content Header (Page header) -->
+        <section class="content-header">
+          <h1>
+           Talent Agency Management System
+
+            <small>it all starts here</small>
+          </h1>
+          <ol class="breadcrumb">
+            <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+            <li class="active">Talent</li>
+          </ol>
+        </section>
+
+        <!-- Main content -->
+<section class="content">
+        <div class="row">
+            <div class="col-md-12">
+              <div class="box">
+                <div class="box-header with-border">
+                  <h3 class="box-title">Add New Talent</h3>
+                  <div class="box-tools pull-right">
+                    <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+                    
+                    <button class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+                  </div>
+                </div><!-- /.box-header -->
+                <div class="box-body">
+                  <div class="row">
+						<!-- Main form start-->
+						<form method="POST" action="" class="form-horizontal" role="form">
+							<div class="col-md-12">
+
+								 
+									<div class="box-header">
+									  <h3 class="box-title">Personal information</h3>
+									</div>
+									<div class="box-body col-md-6 col-md-offset-3">
+									  
+										<div class="form-group">
+										  <label class="control-label col-sm-2" for="email">Full Name:</label>
+										  <div class="col-sm-10">
+											<input type="name" class="form-control" id="full_name" name="full_name" placeholder="Enter full name">
+										  </div>
+										</div>
+									  <div class="form-group">
+										  <label class="control-label col-sm-2" for="email">Email:</label>
+										  <div class="col-sm-10">
+											<input type="email" class="form-control" id="email" placeholder="Enter email">
+										  </div>
+										</div>
+									
+								
+
+
+									</div><!-- /.box-body -->
+								  
+
+								  
+
+							</div>
+						</form>	
+						<!-- //Main form end -->	
+						
+                </div><!-- ./box-body -->
+                <div class="box-footer">
+                  <div class="row">
+
+                  </div><!-- /.row -->
+                </div><!-- /.box-footer -->
+              </div><!-- /.box -->
+            </div><!-- /.col -->
+                      </div><!-- /.row -->
+ <!-- Default box 2-->
+<div class="row">
+   
+          </div><!-- /.row -->
+
+</section><!-- /.content --> 
