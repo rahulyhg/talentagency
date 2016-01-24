6f98264c92dc8803863d1f491af27f3579747b27
Kafia Ahmed <kafia@sutlej.net>
2016-01-25 00:45:30 +0500

    List of languages added

--- dashboard/modules/languages/list_languages.php
+++ dashboard/modules/languages/list_languages.php
@@ -3,25 +3,23 @@
 $tbl = new HTML_Table('', 'data-table table table-striped table-bordered', array('data-title'=>'List of Users'));
 $tbl->addTSection('thead');
 $tbl->addRow();
-$tbl->addCell("<a class='pull btn btn-success btn-md' href ='".$_SERVER['PHP_SELF']."?route=modules/experience_types/add_experience_type'>Add New Experience Type&nbsp;&nbsp;<span class='glyphicon glyphicon-plus'></span></a>");
+$tbl->addCell("<a class='pull btn btn-success btn-md' href ='".$_SERVER['PHP_SELF']."?route=modules/list_languages/add_language'>Add New Language&nbsp;&nbsp;<span class='glyphicon glyphicon-plus'></span></a>");
 $tbl->addRow();
-$tbl->addCell('Type ID', '', 'header');
-$tbl->addCell('Type Name', '', 'header');
-$tbl->addCell('Description', '', 'header');
+$tbl->addCell('Language ID', '', 'header');
+$tbl->addCell('Language Name', '', 'header');
 $tbl->addCell('History', '', 'header');
 $tbl->addCell('Actions', '', 'header');
 $tbl->addTSection('tbody');
 
  
-$sql = 'SELECT * FROM tams_experience_items WHERE experience_item_status = "active"';
-$get_experience_types = DB::query($sql);
-foreach($get_experience_types as $type) { 
+$sql = 'SELECT * FROM tams_languages WHERE language_status = "active"';
+$get_language = DB::query($sql);
+foreach($get_language as $type) { 
 $tbl->addRow();
-$tbl->addCell($type['experience_item_id']);
-$tbl->addCell($type['experience_item_name']);
-$tbl->addCell($type['experience_item_desc']);
+$tbl->addCell($type['language_id']);
+$tbl->addCell($type['language_name']);
  $tbl->addCell("<p>Created on: <strong> ".getDateTime($type['created_on'],'dtLong')." </strong> by <strong>".get_user_name($type['created_by'])."</strong></p> <p>Last Modified: <strong>".getDateTime($type['last_modified_on'],"dtLong")." </strong> by <strong>".get_user_name($type['last_modified_by'])."</strong></p>  ");
-$tbl->addCell("<a class='pull btn btn-danger btn-xs' href ='".$_SERVER['PHP_SELF']."?route=modules/experience_types/edit_experience_type&experience_item_id=".$type['experience_item_id']."'>Edit Experience Type&nbsp;&nbsp;<span class='glyphicon glyphicon-edit'></span></a>
+$tbl->addCell("<a class='pull btn btn-danger btn-xs' href ='".$_SERVER['PHP_SELF']."?route=modules/languages/edit_language&language_id=".$type['language_id']."'>Edit Language&nbsp;&nbsp;<span class='glyphicon glyphicon-edit'></span></a>
 			   ");
 }
 			  
@@ -31,12 +29,12 @@ $tbl->addCell("<a class='pull btn btn-danger btn-xs' href ='".$_SERVER['PHP_SELF
         <section class="content-header">
           <h1>
           	System Settings
-            <small>list of Experience Types. </small>
+            <small>list of Languages. </small>
           </h1>
           <ol class="breadcrumb">
             <li><a href="<?php echo SITE_ROOT; ?>"><i class="fa fa-dashboard"></i> Home</a></li>
             <li><a href="#">Dashboard</a></li>
-            <li class="active">List Experience Types</li>
+            <li class="active">List Languages</li>
           </ol>
         </section>
 
@@ -46,7 +44,7 @@ $tbl->addCell("<a class='pull btn btn-danger btn-xs' href ='".$_SERVER['PHP_SELF
  <!-- Default box -->
           <div class="box">
             <div class="box-header with-border">
-              <h3 class="box-title">List of Experience Types</h3>
+              <h3 class="box-title">List of Languages</h3>
               <div class="box-tools pull-right">
                 <button class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Open/Close This Box"><i class="fa fa-minus"></i></button><button class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove"><i class="fa fa-times"></i></button>
               </div>
