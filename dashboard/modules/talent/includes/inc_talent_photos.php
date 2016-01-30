7277dbe9057481efee08256947ad272742c5b894
Kafia Ahmed <kafia@sutlej.net>
2016-01-30 16:56:55 +0500

    Header is added to all pages

---
+++ dashboard/modules/talent/includes/inc_talent_photos.php
@@ -0,0 +1,25 @@
+<?php
+
+?>
+<!-- Talent Photos Information box -->       			
+       		<div class="box box-info">
+            <div class="box-header with-border">
+              <h3 class="box-title"> 
+              	Photos Information
+              	 </h3>
+              <div class="box-tools pull-right">
+              		<button class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Open/Close This Box"><i class="fa fa-minus"></i></button>
+              		<button class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove"><i class="fa fa-times"></i></button>
+              </div>
+            </div>
+            
+            <div class="box-body bg-info">
+            <div class="row">
+  					
+							<div class="tab-pane active" id="vitals">
+
+								
+							</div><!--/.tab-pane-->
+				</div> <!--/.row-->
+				</div><!--/.box-body-->
+				</div><!--Photos Information Box-->
