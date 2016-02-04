0204d0954cae8f662f9b4e7cc073b12efcab533d
Kafia Ahmed <kafia@sutlej.net>
2016-02-04 23:24:37 +0500

    search tab

--- dashboard/includes/page-parts/content-default.php
+++ dashboard/includes/page-parts/content-default.php
@@ -1,6 +1,66 @@
 <?php
-		
+// php search data in mysql database using PDO
+// set data in input text
+
+$talent_id = "";
+$first_name = "";
+$last_name = "";
+$dob = "";
+$height_cm = "";
+$weight_kg ="";
+$sex="";
+$nationality="";
+
+if(isset($_POST['Find']))
+{
+        // connect to mysql
+    try {
+        $pdoConnect = new PDO("mysql:host=localhost;dbname=teamsutlej_talent","root","");
+    } catch (PDOException $exc) {
+        echo $exc->getMessage();
+        exit();
+    }
+    
+    // id to search
+  // $talent_id = $_POST['talent_id'];
+    
+     // mysql search query
+    $pdoQuery = "SELECT * FROM tams_talent WHERE talent_id = :talent_id";
+    
+    $pdoResult = $pdoConnect->prepare($pdoQuery);
+    
+    //set your id to the query id
+    $pdoExec = $pdoResult->execute(array(":talent_id"=>$talent_id));
+    
+    if($pdoExec)
+    {
+            // if id exist 
+            // show data in inputs
+        if($pdoResult->rowCount()>0)
+        {
+            foreach($pdoResult as $row)
+            {
+                $talent_id = $row['talent'];
+                $first_name = $row['first_name'];
+                $last_name = $row['last_name'];
+                $dob = $row['dob'];
+				$height_cm = $row['height_cm'];
+				$weight_kg =$row['weight_kg'];
+				$nationality=$row['nationality'];
+            }
+        }
+            // if the id not exist
+            // show a message and clear inputs
+        else{
+            echo 'No Data With This ID';
+        }
+    }else{
+        echo 'ERROR Data Not Inserted';
+    }
+}
+
 ?>
+	
 <!--style>
 	div.xAxis div.tickLabel 
 {    
@@ -71,7 +131,7 @@
 </div>
 
           <div class="row">
-            <div class="col-md-12">
+            <div class="col-md-12 ">
               <div class="box">
                 <div class="box-header with-border">
                   <h3 class="box-title">Search for Talent</h3>
@@ -83,9 +143,37 @@
                 </div><!-- /.box-header -->
                 <div class="box-body">
                   <div class="row">
-                    <div class="col-md-8">
-                       
-                    </div><!-- /.col -->
+             <div class="col-md-8 col-xs-offset-2">
+            <div class="input-group" id="adv-search">
+                <input type="search" class="form-control" placeholder="Search for Talent" />
+                <div class="input-group-btn search-panel">
+                    <div class="btn-group" role="group">
+                        <div class="dropdown dropdown-lg">
+                             <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+                    	<span id="search_concept">Filter by</span> <span class="caret"></span>
+							</button>
+                            <div class="dropdown-menu dropdown-menu-right" role="menu">
+                                <form class="form-horizontal" role="form" action="" method="post">
+                                  <div class="form-group">
+                                    <select class="form-control">
+                                        <option value="0" selected>All Talent</option>
+                                        <option value="1">First Name</option>
+                                        <option value="2">Last Name</option>
+                                        <option value="3">Age</option>
+                                        <option value="4">Height</option>
+										<option value="5">Weight</option>
+										<option value="6">Nationality</option>
+										<option value="7"></option>
+                                    </select>
+                                  </div>
+                                </form>
+                            </div>
+                        </div>
+                        <button type="submit" name="Find" value="Find Data" class="btn btn-primary"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>
+                    </div>
+                </div>
+            </div> 
+                    </div><!-- /.col -->		
                     <div class="col-md-4">
                        
                   </div><!-- /.row -->
@@ -98,6 +186,7 @@
               </div><!-- /.box -->
             </div><!-- /.col -->
                       </div><!-- /.row -->
+					  </div>
  <!-- Default box 2-->
 <div class="row">
    
