4967f720cf42f53d7e79beca96c9c7318c38f0c7
Kafia Ahmed <kafia@sutlej.net>
2016-02-04 14:44:55 +0500

    no error but file is not uploaded to its path

--- dashboard/modules/talent/includes/inc_talent_document.php
+++ dashboard/modules/talent/includes/inc_talent_document.php
@@ -1,10 +1,9 @@
 <?php
+
+
 // List of Documents
-$sql    = "SELECT `document_type_id`,`document_type_name`, `document_type_desc`, `document_type_extension` FROM `tams_document_types` WHERE (`document_type_status` = 'active') AND ( tams_document_types.`document_type_id` NOT IN (SELECT document_type_id FROM tams_talent_documents WHERE talent_id=".$talent_id."))";
+$sql    = "SELECT `document_type_id`,`document_description`, `document_path`, `document_status` FROM `tams_talent_documents` WHERE (`document_status` = 'active') AND ( tams_talent_documents.`document_type_id` NOT IN (SELECT document_type_id FROM tams_talent_documents WHERE talent_id=".$talent_id."))";
 $document_types = DB::query($sql);
-
-
-
 
 if(isset($_GET['talent_id']))
 {
@@ -18,26 +17,79 @@ tams_talent_documents
 WHERE talent_id = $talent_id";
 
 $talent_document = DB::query($document_sql);
-?>
-<style>
+if(isset($_POST['save'])) {
+ 
+$talent_id = $_POST['talent_id'];
+$document_type_id = $_POST['document_type_id'];
+$document_description = $_POST['document_description'];
+$document_path = $_POST['document_path'];
+$document_status = $_POST['document_status']; 
+$last_modified_by = $_SESSION['user_id'];
+$last_modified_on = getDateTime(NULL,"mySQL");
 
-.two{
-	border:0px;
-	margin: -3px -9px 6px 10px;
-   font-size:12px;
+ // if client id is not empty update the database
+ 
+	if($talent_id <> ""){
+		$update = DB::update('tams_talent_documents', array(
+			
+			'talent_id' => $talent_id,
+			'document_type_id' => $document_type_id,
+			'document_description' => $document_description,
+			'document_path' => $document_path,
+			'document_status' => $document_status,
+			'last_modified_by'	=> $last_modified_by,
+			'last_modified_on'	=> $last_modified_on
+			),
+			"talent_id=%s", $talent_id
+		);
+	//check if the file is uploaded and process the file if file is uploaded	
+	
+	if(!file_exists($_FILES['talent_doc']['tmp_name']) || !is_uploaded_file($_FILES['talent_doc']['tmp_name'])) {
+		echo '<h2> No file ploaded</h2>';
+	}  else {
+		echo '<h2> file was uploaded</h2>';
+	}
+	//if logo file is uploaded process the file with upload class
+	
+	$handle1 = new upload($_FILES['talent_doc']);
+		if ($handle1->uploaded) {
+			$handle1->file_new_name_body   = $talent_id.'_doc';
+			$handle1->allowed = array('application/pdf','application/msword','application/vnd.ms-powerpoint', 'application/vnd.ms-excel','text/plain');
+			$handle->file_new_name_ext = 'pdf';
+			$handle1->file_overwrite = true;
+			$handle1->process('..uploads/documents/');
+		if ($handle1->processed) {
+			
+	// save uploaded file name and path in database table field logo_url
+	
+	
+			$last_modified_by = $_SESSION['user_id'];
+			$last_modified_on = getDateTime(NULL,"mySQL");
+			
+	/* if client id is not empty update the database */
+	
+		if($talent_id <> ""){
+				$update = DB::update('tams_talent_documents', array(
 
-	width:20px;
-	height:20px;
-	color:black;
-	background-color: #00C0EF;
+				'document_path'=> '/talent/uploads/documents/'.$talent_id.'_doc.pdf',
+				'last_modified_by'	=> $last_modified_by,
+				'last_modified_on'	=> $last_modified_on
+			),
+			"talent_id=%s", $talent_id
+		);
+		
+		}
+		echo 'Photo is uploaded and path select saved in database';	
+			$handle1->clean();
+		} else {
+			echo 'error : ' . $handle1->error;
+		} // close handle processed
+		} // close handle uploaded
+		} // close file exist
+				
 }
-.one {
-margin-left:20px;
-font-size:19px;
-
-}   
-</style>
-<form id="edit_talent_document_info" name="edit_talent_document_info" class="form-horizontal" method="post" action="process_talent_forms.php?talent_id="<?php echo $talent_id; ?>" >
+?>
+<form  enctype="multipart/form-data" id="edit_talent_document_info" name="edit_talent_document_info" class="form-horizontal" method="post" action="process_talent_forms.php?talent_id="<?php echo $talent_id; ?>" >
 <!-- Documents Information box -->       			
        		<div class="box box-info">
             <div class="box-header with-border">
@@ -52,87 +104,55 @@ font-size:19px;
             
             <div class="box-body bg-info">
             <div class="row">
-			
-			<?php
-		if($talent_document )
-		{
-		?>
-		<p class="one">
-		<?php 
-		foreach($talent_document as $document){
-		?>				
-		<span class="label label-info" style="display:inline-block;">
-		<button style="float:right" class="two"><a href="#"></a><i class="fa fa-times"></i></button>
-			<?php echo get_document_type_name($document['document_type_id']); ?>
-		</span>	
-			
+				<div class="form-group"  >
+						<label class="col-md-3 col-sm-3 control-label"> Document Name:</label>
+						  <div class="col-md-9 col-sm-9">
+							 <input class="form-control" type="text" required placeholder="Add Document Name " 
+							 value="" name="document_name" id="document_name">							
+						  </div>
+					</div>
 
-		<?php
-		} // for each $talent_document									
-		?>
-			
-		</p> 
-
-		 <?php
-
-		}  // End if $talent_document
-
-		?>
-
-		<?php
-		if($document_types )
-		{
-			?>
-			
-  					<div class="form-group">
-					<label class="col-md-3 col-sm-3 control-label">
-							Documents :
-					</label>
+                    <div class="form-group"  >
+						<label class="col-md-3 col-sm-3 control-label"> Document Description:</label>
+						  <div class="col-md-9 col-sm-9">
+							 <input class="form-control" type="text" required placeholder="Add Document Description " 
+							 value="" name="document_description" id="document_description">							
+						  </div>
+					</div>
+					 
+				<div class="form-group">
+						<label class="col-md-3 col-sm-3 control-label">
+							Upload Document :
+						</label>
 						<div class="col-md-9 col-sm-9">
-						<div class="input-group">
-						<select name="document_type_id" id="document_type_id" class=" input-group form-control  select2"  style="padding:5px;"  >
-					
-							<option value="">
-								Select a Document type
-							</option>
-	
-							<?php 
-							foreach($document_types as $type){
-								?>	
-							<option value="<?php echo $type['document_type_id'];?>">
-								<?php echo $type['document_type_name'];?>
-							</option>
-							<?php
-							} // for each document type									
-							?>
-
-						</select>
-						<div class="input-group-addon"> <button type="submit" class='btn btn-xs pull-right' name="save" value="save">
-							Add &nbsp;
-							<i class="fa fa-plus">
-							</i>
-					</button>
-					</div>
+						<img src="<?php echo $photo1_url; ?>" alt="no file uploaded" />
+		<!-- input-group image-preview [FROM HERE]-->
+            <div class="input-group image-preview">
+                <input type="text" class="form-control image-preview-filename" disabled="disabled"> <!-- don't give a name === doesn't send on POST/GET -->
+                <span class="input-group-btn">
+                    <!-- image-preview-clear button -->
+                    <button  type="submit" class="btn btn-default image-preview-clear" style="display:none;">
+                        <span class="glyphicon glyphicon-remove"></span> Clear
+                    </button>
+                    <!-- image-preview-input -->
+                    <div class="btn btn-default image-preview-input">
+                        <span class="glyphicon glyphicon-folder-open"></span>
+                        <span class="image-preview-input-title">Browse</span>
+                        <input type="file" accept="application/msword, application/vnd.ms-excel, application/vnd.ms-powerpoint,
+text/plain, application/pdf" name="talent_doc" id="talent_doc"/> <!-- Form Upload Field -->
+                    </div>
+                    <button type="button" name="save"  class="btn btn-labeled btn-default"> <span class="btn-label"><i class="glyphicon glyphicon-upload"></i> </span>Upload</button>
+                </span>
+            </div><!-- /input-group image-preview [TO HERE]-->
 						</div>
-				
-				 
+					</div>		
 					
-					
-
-					</div>
-					<script type="text/javascript">
+  					
+<script type="text/javascript">
 	$(".select2").select2();
 	
 </script>
-
-
-			</div> <!--/.form-group-->
- <?php
-
-		}  // End if $document_type
-
-?>				
-				
+							
 				</div> <!--/.row-->
 				<div class="box-footer">
  								<div class="form-group">
@@ -142,6 +162,11 @@ font-size:19px;
 							<i class="fa fa-chevron-circle-right">
 							</i>
 						</a>
+					<button style="margin-right:10px;" type="submit" class='btn btn-success btn-lg pull-right' name="save" value="save">
+							Save &nbsp;
+							<i class="fa fa-chevron-circle-right">
+							</i>
+					</button>
 					</div>	<!-- /.col -->
 				</div>		<!-- /form-group -->
 				<small>
