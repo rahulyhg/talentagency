5c5e75cfbbc8afe9f73183a6c9ca71c0099436ee
Kafia Ahmed <kafia@sutlej.net>
2016-02-10 11:26:31 +0500

    content-default File clean up

--- dashboard/includes/page-parts/content-default.php
+++ dashboard/includes/page-parts/content-default.php
@@ -1,63 +1,4 @@
 <?php
-// php search data in mysql database using PDO
-// set data in input text
-
-$talent_id = "";
-$first_name = "";
-$last_name = "";
-$dob = "";
-$height_cm = "";
-$weight_kg ="";
-$sex="";
-$nationality="";
-
-if(isset($_POST['Find']))
-{
-        // connect to mysql
-    try {
-        $pdoConnect = new PDO("mysql:host=localhost;dbname=teamsutlej_talent","root","");
-    } catch (PDOException $exc) {
-        echo $exc->getMessage();
-        exit();
-    }
-    
-    // id to search
-  // $talent_id = $_POST['talent_id'];
-    
-     // mysql search query
-    $pdoQuery = "SELECT * FROM tams_talent WHERE talent_id = :talent_id";
-    
-    $pdoResult = $pdoConnect->prepare($pdoQuery);
-    
-    //set your id to the query id
-    $pdoExec = $pdoResult->execute(array(":talent_id"=>$talent_id));
-    
-    if($pdoExec)
-    {
-            // if id exist 
-            // show data in inputs
-        if($pdoResult->rowCount()>0)
-        {
-            foreach($pdoResult as $row)
-            {
-                $talent_id = $row['talent'];
-                $first_name = $row['first_name'];
-                $last_name = $row['last_name'];
-                $dob = $row['dob'];
-				$height_cm = $row['height_cm'];
-				$weight_kg =$row['weight_kg'];
-				$nationality=$row['nationality'];
-            }
-        }
-            // if the id not exist
-            // show a message and clear inputs
-        else{
-            echo 'No Data With This ID';
-        }
-    }else{
-        echo 'ERROR Data Not Inserted';
-    }
-}
 
 ?>
 	
@@ -141,43 +82,7 @@ if(isset($_POST['Find']))
                     <button class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
                   </div>
                 </div><!-- /.box-header -->
-                <div class="box-body">
-                  <div class="row">
-             <div class="col-md-8 col-xs-offset-2">
-            <div class="input-group" id="adv-search">
-                <input type="search" class="form-control" placeholder="Search for Talent" />
-                <div class="input-group-btn search-panel">
-                    <div class="btn-group" role="group">
-                        <div class="dropdown dropdown-lg">
-                             <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
-                    	<span id="search_concept">Filter by</span> <span class="caret"></span>
-							</button>
-                            <div class="dropdown-menu dropdown-menu-right" role="menu">
-                                <form class="form-horizontal" role="form" action="" method="post">
-                                  <div class="form-group">
-                                    <select class="form-control">
-                                        <option value="0" selected>All Talent</option>
-                                        <option value="1">First Name</option>
-                                        <option value="2">Last Name</option>
-                                        <option value="3">Age</option>
-                                        <option value="4">Height</option>
-										<option value="5">Weight</option>
-										<option value="6">Nationality</option>
-										<option value="7"></option>
-                                    </select>
-                                  </div>
-                                </form>
-                            </div>
-                        </div>
-                        <button type="submit" name="Find" value="Find Data" class="btn btn-primary"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>
-                    </div>
-                </div>
-            </div> 
-                    </div><!-- /.col -->		
-                    <div class="col-md-4">
-                       
-                  </div><!-- /.row -->
-                </div><!-- ./box-body -->
+           
                 <div class="box-footer">
                   <div class="row">
 
