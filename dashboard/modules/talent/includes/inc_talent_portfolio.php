ed0f3c37a5532b50acfba141ba7aa345a9952736
Rana Mansoor Akbar Khan <mansoor@sutlej.net>
2016-02-15 04:03:51 +0500

    Portfolio Form Updated

--- dashboard/modules/talent/includes/inc_talent_portfolio.php
+++ dashboard/modules/talent/includes/inc_talent_portfolio.php
@@ -18,74 +18,8 @@ tams_talent_portfolio
 WHERE talent_id = $talent_id";
 
 $talent_portfolio_item = DB::query($portfolio_sql);
-
-if(isset($_POST['save'])) {
  
-$talent_id = $_POST['talent_id'];
-$portfolio_item_id = $_POST['portfolio_item_id'];
-$portfolio_item_status = $_POST['portfolio_item_status']; 
-$last_modified_by = $_SESSION['user_id'];
-$last_modified_on = getDateTime(NULL,"mySQL");
-
- // if talent id is not empty update the database
  
-	if($talent_id <> ""){
-		$update = DB::update('tams_talent_portfolio', array(
-			
-			'talent_id' => $talent_id,
-			'portfolio_item_id' => $portfolio_item_id,
-			'portfolio_item_status' => $portfolio_item_status,
-			'last_modified_by'	=> $last_modified_by,
-			'last_modified_on'	=> $last_modified_on
-			),
-			"talent_id=%s", $talent_id
-		);
-	//check if the file is uploaded and process the file if file is uploaded	
-	
-	if(!file_exists($_FILES['talent_portfolio']['tmp_name']) || !is_uploaded_file($_FILES['talent_portfolio']['tmp_name'])) {
-		echo '<h2> No file uploaded</h2>';
-	}  else {
-		echo '<h2> file was uploaded</h2>';
-	}
-	//if logo file is uploaded process the file with upload class
-	
-	$handle = new upload($_FILES['talent_portfolio']);
-		if ($handle->uploaded) {
-			$handle->file_new_name_body   = $talent_id.'_portfolio';
-			$handle->allowed = array('application/pdf','application/msword','text/plain', 'text/rtf', 'image/*','application/zip', 'audio/mp3','audio/mpeg', 'audio/mpeg3', 'audio/x-mpeg-3', 'video/mpeg', 'video/mp4', 'video/quicktime', 'video/x-ms-wmv', 'application/x-rar-compressed');
-			$handle->file_overwrite = true;
-			$handle->process('../uploads/portfolio/');
-		if ($handle->processed) {
-			
-	// save uploaded file name and path in database table field logo_url
-	
-	
-			$last_modified_by = $_SESSION['user_id'];
-			$last_modified_on = getDateTime(NULL,"mySQL");
-			
-	/* if talent id is not empty update the database */
-	
-		if($talent_id <> ""){
-				$update = DB::update('tams_talent_portfolio', array(
-
-				'portfolio_item_url'=> '/talent/uploads/portfolio/'.$talent_id.'_portfolio',
-				'portfolio_item_description' =>$_POST['portfolio_item_description'],
-				'last_modified_by'	=> $last_modified_by,
-				'last_modified_on'	=> $last_modified_on
-			),
-			"talent_id=%s", $talent_id
-		);
-		
-		}
-		echo 'File is uploaded and path select saved in database';	
-			$handle->clean();
-		} else {
-			echo 'error : ' . $handle->error;
-		} // close handle processed
-		} // close handle uploaded
-		} // close file exist
-				
-}
 ?>
 
 <form enctype="multipart/form-data" id="edit_talent_portfolio_info" name="edit_talent_portfolio_info" class="form-horizontal" method="post" action="process_talent_forms.php?talent_id=<?php echo $talent_id; ?>" >
@@ -93,7 +27,7 @@ $last_modified_on = getDateTime(NULL,"mySQL");
        		<div class="box box-info">
             <div class="box-header with-border">
               <h3 class="box-title"> 
-              	Portfolio Information
+              	Portfolio Links
               	 </h3>
               <div class="box-tools pull-right">
               		<button class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Open/Close This Box"><i class="fa fa-minus"></i></button>
@@ -110,7 +44,21 @@ $last_modified_on = getDateTime(NULL,"mySQL");
 		?>
 		<?php 
 		foreach($talent_portfolio_item as $portfolio){
-		?>				
+		$portfolio_item_name = DB::queryFirstField("SELECT portfolio_item_name from tams_portfolio_items  WHERE portfolio_item_id = ".$portfolio['portfolio_item_id']);	
+		?>
+ 
+ 		<div class="form-group">
+	 		<div class="col-md-3 col-sm-3">
+	 		<strong><?php echo $portfolio_item_name; ?> : </strong>
+	 		</div>
+	 		<div class="col-md-3 col-sm-3">
+	 		<?php echo $portfolio['portfolio_item_description']; ?>
+	 		</div>
+	 		<div class="col-md-6 col-sm-6">
+	 		 <a target="_blank" href="<?php echo $portfolio['portfolio_item_url']; ?>" ><i class="fa fa-open"></i> &nbsp;<?php echo $portfolio['portfolio_item_url']; ?></a>
+	 		</div> 		 
+ 
+		</div>	
 		<?php
 		} // for each $talent_portfolio									
 		?>
@@ -119,13 +67,11 @@ $last_modified_on = getDateTime(NULL,"mySQL");
 
 		}  // End if $talent_portfolio
 
-		?>
+ ?>
 
-		<?php
-		if($portfolio_items )
-		{
-			?>
-			
+			<div class="form-group">
+			<hr>
+			</div>
   					<div class="form-group"  >
 						<label class="col-md-3 col-sm-3 control-label"> Portfolio Type:</label>
 						  <div class="col-md-9 col-sm-9">
@@ -147,11 +93,7 @@ $last_modified_on = getDateTime(NULL,"mySQL");
 							?> 
 
 						</select>
-						<div class="input-group-addon"> <button type="submit" class='btn btn-xs pull-right' name="save" value="save">
-							Add &nbsp;
-							<i class="fa fa-plus">
-							</i>
-					</button>
+						 
 					</div>
 						</div>
 	
@@ -164,32 +106,21 @@ $last_modified_on = getDateTime(NULL,"mySQL");
 							 value="" name="portfolio_item_description" id="portfolio_item_description">							
 						  </div>
 						  </div>
-						  <div class="form-group">
-						<label class="col-md-3 col-sm-3 control-label">
-							Upload Portfolio :
-						</label>
-						<div class="col-md-9 col-sm-9">
-					
-		<!-- input-group image-preview [FROM HERE]-->
-            <div class="input-group image-preview">
-                <input type="text" class="form-control image-preview-filename" disabled="disabled"> <!-- don't give a name === doesn't send on POST/GET -->
-                <span class="input-group-btn">
-                    <!-- image-preview-clear button -->
-                    <button  type="submit" class="btn btn-default image-preview-clear" style="display:none;">
-                        <span class="glyphicon glyphicon-remove"></span> Clear
-                    </button>
-                    <!-- image-preview-input -->
-                    <div class="btn btn-default image-preview-input">
-                        <span class="glyphicon glyphicon-folder-open"></span>
-                        <span class="image-preview-input-title">Browse</span>
-                        <input type="file" accept="application/pdf,application/msword,text/plain, text/rtf, image/*,application/zip, audio/mp3,audio/mpeg,
-						audio/mpeg3, audio/x-mpeg-3, video/mpeg, video/mp4, video/quicktime, video/x-ms-wmv, application/x-rar-compressed" name="talent_portfolio" id="talent_portfolio"/> <!-- Form Upload Field -->
-                    </div>
-                    <button type="button" name="save"  class="btn btn-labeled btn-default"> <span class="btn-label"><i class="glyphicon glyphicon-upload"></i> </span>Upload</button>
-                </span>
-            </div><!-- /input-group image-preview [TO HERE]-->
-						</div>
-					</div>	
+							<div class="form-group">
+								<label class="col-md-3 col-sm-3 control-label">
+									Portfolio URL:
+								</label>
+								<div class="col-md-9 col-sm-9">
+									<div class="input-group">
+
+										<input   class=" input-group form-control"     type="url" required value="" placeholder="Enter URL of Portfolio Item" name="portfolio_item_url" id="portfolio_item_url">
+										<div class="input-group-addon">
+											<i class="fa fa-globe">
+											</i>
+										</div>
+									</div>
+								</div>
+							</div>
 						
 <script type="text/javascript">
 	$(".select2").select2();
@@ -198,11 +129,7 @@ $last_modified_on = getDateTime(NULL,"mySQL");
 
 
 		
- <?php
-
-		}  // End if $portfolio_items
-
-?>				
+				
 				
 				</div> <!--/.row-->
 				<div class="box-footer">
