93155f7b701cfda297d4d42bde86b18cd860a928
Rana Mansoor Akbar Khan <mansoor@sutlej.net>
2016-03-07 13:37:45 +0500

    Fixed process talent deletes

--- dashboard/process_talent_deletes.php
+++ dashboard/process_talent_deletes.php
@@ -7,8 +7,8 @@ if(isset($_GET['action'])) {
 
 	case "delete_experience_item":
 		$talent_id = $_GET['talent_id'];
-		$id = $_GET['talent_experience_item_id'];
-		$delete= mysql_query("DELETE FROM tams_talent_experience WHERE talent_experience_item_id='$id'");	
+		$id = $_GET['id'];	
+		DB::delete('tams_talent_experience', "talent_experience_item_id=%s AND talent_id=%s", $id, $talent_id);
 		
 		header('Location: index.php?route=modules/talent/edit_talent_profile&talent_id='.$talent_id.'#experience');		
 		break;
