bee1bd18d51d78906ccc90756eb4a338c0f921e8
Kafia Ahmed <kafia@sutlej.net>
2016-03-08 16:00:21 +0500

    Delete all photos,documents,languages,experiences,portfolio & notes records from DB on deleting talent

--- dashboard/process_delete_talent.php
+++ dashboard/process_delete_talent.php
@@ -4,12 +4,66 @@ require_once('functions.php');
 if(isset($_GET['action'])) {
 
 switch($_GET['action']){
+	
+/*********************************************************
+* 
+***************  DELETE TALENT FROM TAMS_TALENT  **********************
+* 
+***********************************************************/
+	
 		case "delete_talent":
 		$id = $_GET['id'];	
 		DB::delete('tams_talent', "talent_id=%s", $id);
-		//TO Do Delete Images & documents from directory/folder 
+/*********************************************************
+* 
+***************   DELETE TALENT EXPERIENCE   ************** 
+* 
+***********************************************************/
+		DB::delete('tams_talent_experience', "talent_id=%s", $id);
+		
+/*********************************************************
+* 
+***************   DELETE TALENT LANGUAGE   ************** 
+* 
+***********************************************************/
+		DB::delete('tams_talent_language', "talent_id=%s", $id);
+		
+/*********************************************************
+* 
+***************   DELETE TALENT NOTES   ************** 
+* 
+***********************************************************/	
+		DB::delete('tams_talent_comments', "talent_id=%s", $id);
+		
+/*********************************************************
+* 
+***************   DELETE PORTFOLIO ITEM   ************** 
+* 
+***********************************************************/
+		DB::delete('tams_talent_portfolio', "talent_id=%s", $id);
+/*********************************************************
+* 
+***************   DELETE TALENT PHOTO   ************** 
+* 
+***********************************************************/
+		DB::delete('tams_talent_photos', "talent_id=%s", $id);
+		
+/*********************************************************
+* 
+***************   DELETE DOCUMENT   ************** 
+* 
+***********************************************************/
+		DB::delete('tams_talent_documents', "talent_id=%s", $id);
+
+/*********************************************************
+* 
+*************** //TO Do Delete Images & documents from folder ************** 
+* 
+***********************************************************/		
 		header('Location: index.php?route=modules/talent/view_talents');		
 		break;
+		
+	 
 }
 } else {
 	header('Location: index.php');
