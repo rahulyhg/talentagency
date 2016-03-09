70966a23455655c39582a7a50b6a87a27b0883aa
Kafia Ahmed <kafia@sutlej.net>
2016-03-09 13:07:05 +0500

    Added availability of talent in profile card and some css changes

--- dashboard/modules/talent_lists/search_talents.php
+++ dashboard/modules/talent_lists/search_talents.php
@@ -53,28 +53,28 @@ $get_talents = DB::query($sql);
 				
 				   <input type="text" class="serchform" name="x" placeholder="Search term...">
 				  	
-				 <Select NAME="field" class="field"><option selected>Filter by</option>
-						<Option VALUE="first_name">First Name</option>
-						<Option VALUE="last_name">Last Name</option>
-						<Option VALUE="nationality">Nationality</option>
-						<Option VALUE="sex">Gender</option>
-						<Option VALUE="dob">Age</option>
-						<Option VALUE="eye_color">Eye color</option>
-						<Option VALUE="height_cm">Height</option>
-						<Option VALUE="weight">Weight</option>
-						<Option VALUE="language">Language</option>
-						<Option VALUE="experience">Skill</option>
-						</select>
+				 <select name="field" class="field"><option selected>Filter by</option>
+						<option value="first_name">First Name</option>
+						<option value="last_name">Last Name</option>
+						<option value="nationality">Nationality</option>
+						<option value="sex">Gender</option>
+						<option value="dob">Age</option>
+						<option value="eye_color">Eye color</option>
+						<option value="height_cm">Height</option>
+						<option value="weight">Weight</option>
+						<option value="language">Language</option>
+						<option value="experience">Skill</option>
+				</select>
 					 <span class="dropdown">
                 
-				 <Select NAME="field" class="butn"><option selected>Operator by</option>
-						  <option><a href="#">Contains</a></option>
-						  <option><a href="#">It's equal</a></option>
-						  <option><a href="#">Greather than ></a></option>
-						   <option><a href="#less_than">Less than < </a></option>
-						   <option><a href="#between">Between</a></option>
-						    <option><a class="#like">Like %</a></option>
-						</select>
+				 <select name="field" class="butn"><option selected>Operator</option>
+						  <option>Contains</option>
+						  <option>It's equal</option>
+						  <option>Greather than > </option>
+						  <option>Less than < </option>
+						  <option>Between</option>
+						  <option>Like %</option>
+				</select>
 					  
 
 					 
@@ -98,22 +98,37 @@ foreach($get_talents as $talent) {
             <!-- Add the bg color to the header using any of the bg-* classes -->
             <div class="widget-user-header bg-aqua-active">
               <h3 class="widget-user-username"><?php echo $talent['first_name']." ".$talent['last_name']; ?></h3>
-              <h5 class="widget-user-desc"><?php echo $talent['sex'];?>,&nbsp;<?php echo getAge($talent['dob']);  ?> Years</h5>
+              <h5 class="widget-user-desc"><?php echo $talent['sex'].",&nbsp;". getAge($talent['dob']);  ?> Years</h5>
             </div>
             <div class="widget-user-image">
             <img class="img-circle" src="<?php echo $talent['photo1_url']; ?>"  alt="talent_photo"  />
             </div>
             <div class="box-footer">
             <div class="row">
-            
+            <?php
+			$events = $talent['events'];
+				if (is_null($events) OR $events == "" ) {
+					$events = " - not set - ";
+				} 
+				elseif($events == 1 ){
+					$events = "Yes";
+				}
+				else {
+					$events ="No"; 
+				} 
+
+			
+			?>
                   
               <ul class=" nav nav_stacked">
-			   <li><h4 style="color:black;">Nationality</h4></li>
+			   <li><h4>Nationality</h4></li>
 			   <li ><?php echo $talent['nationality'];?></li>
-                <li><h4 style="color:black;">Professional Skills</h4></li>
+                <li><h4>Professional Skills</h4></li>
                 <li><?php echo list_talent_experience($talent['talent_id']);?></li>
-                <li><h4 style="color:black;">spoken Languages</h4></li>
+                <li><h4>spoken Languages</h4></li>
                 <li><?php echo list_talent_language($talent['talent_id']);?></li>
+				<li><h4>Available for Events?</h4></li>
+				<li><?php echo $events;?></li>
               </ul>
             </div>
               <div class="row">
@@ -130,7 +145,7 @@ foreach($get_talents as $talent) {
                 <div class="col-sm-4 border-right">
                   <div class="description-block">
                     <span class="description-text">
-					<a class='btn btn-info btn-circle btn-lg'title="View Profile" href ="index.php?route=modules/talent/view_talent_profile&talent_id=<?php echo $talent['talent_id']; ?>">&nbsp;&nbsp;<span class='glyphicon glyphicon-user'></span></a>
+					<a class='btn btn-info btn-circle btn-lg' title="View Profile" href ="index.php?route=modules/talent/view_talent_profile&talent_id=<?php echo $talent['talent_id']; ?>">&nbsp;&nbsp;<span class='glyphicon glyphicon-user'></span></a>
 					</span>
                   </div>
                   <!-- /.description-block -->
