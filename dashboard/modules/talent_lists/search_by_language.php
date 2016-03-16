8542e5f658e6d352bb9c4c0a6490d40022e79232
Naina-khawar <naina@sutlej.net>
2016-03-16 15:49:52 +0500

    changed the option field and replace with drop down option .

--- dashboard/modules/talent_lists/search_by_language.php
+++ dashboard/modules/talent_lists/search_by_language.php
@@ -5,8 +5,9 @@ if(isset($_GET['query'])) {
 
 if (trim($_GET{'query'}) <> "" ){
 $query = trim(strtolower($_GET{'query'}));
-$sql = 'SELECT * FROM tams_talent_language WHERE ';
-$sql .= "( LOWER(language) LIKE '%".$query."%' ) ";
+
+$sql = 'SELECT * FROM tams_languages WHERE ';
+$sql .= "( LOWER(language_name) LIKE '%".$query."%' ) ";
 $get_talents = DB::query($sql);
 
 
@@ -60,10 +61,96 @@ $get_talents = DB::query($sql);
 
 					  
 
-			    <input type="text" class="serchform" name="query" placeholder="Enter Talent Name.." />
-				  	
-					 
-				 <input  type="submit" class="searchbutn" name="submit" value="Search" />
+			    <select id="language" name="query" class="field"   >
+										<option value="" selected="selected" > -- Select Language -- </option>
+											<option value="	English (United States)" <?php if($language== "	English (United States)"){ echo 'selected = "selected" ';}?> >
+													English (United States)
+											</option>
+											<option value="Arabic" <?php if($language== "	Arabic"){ echo 'selected = "selected" ';}?> >
+														Arabic
+											</option>
+											<option value="	Urdu" <?php if($language== "	Urdu"){ echo 'selected = "selected" ';}?> >
+												 	Urdu
+											</option>
+											<option value="		Sinhala" <?php if($language== "		Sinhala"){ echo 'selected = "selected" ';}?> >
+													Sinhala
+											</option>
+											<option value="Russian" <?php if($language== "	Russian"){ echo 'selected = "selected" ';}?> >
+												   Russian
+											</option>
+											<option value="	Hindi" <?php if($language== "	Hindi"){ echo 'selected = "selected" ';}?> >
+													Hindi
+											</option>
+											<option value="		Malayalam" <?php if($language== "		Malayalam"){ echo 'selected = "selected" ';}?> >
+													Malayalam
+											</option>
+											<option value="	Tamil" <?php if($language== "		Tamil"){ echo 'selected = "selected" ';}?> >
+												    Tamil
+											</option>
+											<option value="		Farsi" <?php if($language== "		Farsi"){ echo 'selected = "selected" ';}?> >
+													Farsi
+											</option>
+											<option value="	German" <?php if($language== "		German"){ echo 'selected = "selected" ';}?> >
+												  	German
+											</option>
+											<option value="	French" <?php if($language== "		French"){ echo 'selected = "selected" ';}?> >
+													French
+											</option>
+											<option value="		Bengali" <?php if($language== "		Bengali"){ echo 'selected = "selected" ';}?> >
+													Bengali
+											</option>
+											<option value="		Spanish" <?php if($language== "		Spanish"){ echo 'selected = "selected" ';}?> >
+													Spanish
+											</option>
+											<option value="		Portuguese" <?php if($language== "	Portuguese"){ echo 'selected = "selected" ';}?> >
+													Portuguese
+											</option>
+											<option value="		Swahili)" <?php if($language== "		Swahili"){ echo 'selected = "selected" ';}?> >
+													Swahili
+											</option>
+											<option value="		Thai" <?php if($language== "		Thai"){ echo 'selected = "selected" ';}?> >
+													Thai
+											</option>
+											<option value="	Tagalog" <?php if($language== "		Tagalog"){ echo 'selected = "selected" ';}?> >
+													Tagalog
+											</option>
+											<option value="	Finnish)" <?php if($language== "		Finnish"){ echo 'selected = "selected" ';}?> >
+													Finnish
+											</option>
+											<option value="	Afrikaans" <?php if($language== "		Afrikaans"){ echo 'selected = "selected" ';}?> >
+													Afrikaans
+											</option>
+											<option value="		Bahasa" <?php if($language== "		Bahasa"){ echo 'selected = "selected" ';}?> >
+													Bahasa
+											</option>
+											<option value="		Italian" <?php if($language== "		Italian") {echo 'selected = "selected" ';}?> >
+													Italian
+											</option>
+											<option value="		Polish" <?php if($language== "		Polish"){ echo 'selected = "selected" ';}?> >
+													Polish
+											</option>
+											<option value="		Chinese" <?php if($language== "		Chinese"){ echo 'selected = "selected" ';}?> >
+													Chinese
+											</option>
+											<option value="	Hungarian" <?php if($language== "		Hungarian"){ echo 'selected = "selected" ';}?> >
+													Hungarian
+											</option>
+											<option value="	Dutch" <?php if($language== "		Dutch"){ echo 'selected = "selected" ';}?> >
+														Dutch
+											</option>
+											<option value="		Twi" <?php if($language== "	Twi"){ echo 'selected = "selected" ';}?> >
+													Twi
+											</option>
+											<option value="		Polish" <?php if($language== "		Polish"){ echo 'selected = "selected" ';}?> >
+													Polish
+											</option>
+											<option value="		Turkish" <?php if($language== "		Turkish"){ echo 'selected = "selected" ';}?> >
+													Turkish
+											</option>
+											
+										</select>
+				  					 <input  type="submit" class="searchbutn" name="submit" value="Search" />
+
 				 
 			</form>
   	</p>
